b4823f96aec618bf6085f925a36069d55f1663c1
Brendon Telman <b.telman01@gmail.com>
2021-01-18 23:28:34 -0600

    Fix issues with scripts. Remove some steps from setup script

--- scripts/setup-wsl.sh
+++ scripts/setup-wsl.sh
@@ -3,33 +3,5 @@ cd ~
 sudo apt-get update
 sudo apt-get install -y git gperf flex bison libncurses5-dev gcc-arm-none-eabi python-pip
 sudo pip install pyelftools
-
-#openocd
 sudo apt-get install -y libusb-1.0-0-dev libftdi-dev libtool autoconf texinfo
-git clone https://github.com/MotorolaMobilityLLC/openocd
-cd openocd
-git submodule init
-git submodule update
-./bootstrap
-./configure --prefix=/usr/local
-make
-sudo make install
-cd -
-
-#GDB
 sudo apt-get install -y libexpat1-dev zlib1g-dev guile-2.0-dev python2.7-dev
-wget http://ftp.gnu.org/gnu/gdb/gdb-7.11.tar.gz
-tar -zxf gdb-7.11.tar.gz
-cd gdb-7.11
-./configure --prefix=/usr/local --program-prefix=arm-none-eabi- --target=arm-none-eabi --with-python --with-guile
-make
-sudo make install
-
-#nuttx kconfig-mconf
-git clone https://github.com/MotorolaMobilityLLC/nuttx && \
-cd ./nuttx/ && \
-./configure --enable-mconf --disable-nconf --disable-gconf --disable-qconf && \
-make && \
-sudo make install && \
-sudo ldconfig
-cd -
